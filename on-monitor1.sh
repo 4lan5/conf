67f74af7844f4810ed254f7622532af6c62eee5d
4lan5 <alans.zh@gmail.com>
2017-11-15 09:14:52 +0800

    fix xrandr 1.5.0

--- on-monitor1.sh
+++ on-monitor1.sh
@@ -3,6 +3,6 @@
 #turn on monitor1 and reset wallpaper in i3wm
 
 
-xrandr --output  LVDS1  --auto  --output  HDMI-1 --auto --right-of LVDS-1 
+xrandr --output  LVDS-1  --auto  --output  HDMI-1 --auto --right-of LVDS-1 
 
 feh --bg-scale ~/Pictures/WALLPAPER/SZE.jpg
